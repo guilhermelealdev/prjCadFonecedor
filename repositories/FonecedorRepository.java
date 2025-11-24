3fb2b026648f50380740830494153d3afd2b649c
GuilhermeLealDev <guilherme.leal.dev2025@gmail.com>
2025-11-24 08:43:50 -0300

    Delete repositories directory

--- repositories/FonecedorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.guilhermef.prjCadFonecedor.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.guilhermef.prjCadFonecedor.entities.Fornecedor;
-
-public interface FonecedorRepository extends JpaRepository<Fornecedor, Long>{
-
-}
