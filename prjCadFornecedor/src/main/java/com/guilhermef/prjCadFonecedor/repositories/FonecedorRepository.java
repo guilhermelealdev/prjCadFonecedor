6c4b98873c12f1e58c2007b9bae16bd264009a6d
GuilhermeLealDev <guilherme.leal.dev2025@gmail.com>
2025-11-24 07:21:17 -0300

    Adição de Queries

--- prjCadFornecedor/src/main/java/com/guilhermef/prjCadFonecedor/repositories/FonecedorRepository.java
+++ prjCadFornecedor/src/main/java/com/guilhermef/prjCadFonecedor/repositories/FonecedorRepository.java
@@ -1,9 +1,18 @@
 package com.guilhermef.prjCadFonecedor.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.guilhermef.prjCadFonecedor.entities.Fornecedor;
 
 public interface FonecedorRepository extends JpaRepository<Fornecedor, Long>{
-
+	
+	List<Fornecedor> encontrarPorRazaoSocial(String razaoSocial);
+	List<Fornecedor> encontrarPorNomeFantasia(String nomeFantasia);
+	List<Fornecedor> encontrarPorCnpj(String cnpj);
+	List<Fornecedor> encontrarPorEndereco(String endereco);
+	List<Fornecedor> encontrarPorTelefone(String telefone);
+	List<Fornecedor> encontrarPorInscricaoEstadual(String inscricaoEstadual);
+	
 }
